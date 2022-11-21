752faab283c79a1e19a420fb241c4d201033a44d
NickdevC <nick_coles@msn.com>
2022-11-21 20:32:26 +0000

    Destructuring using rest parameter

--- destructuring.js
+++ destructuring.js
@@ -24,5 +24,33 @@ console.log(mike, jill, alicia);
 let languages = ["english", "french", "spanish", "german", "japanese"];
 let [johnNative, johnSecondary] = languages;
 console.log(johnNative, johnSecondary);
+// Using commas to skip values
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese",
+};
+let { firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
 
 // Using rest parameter syntax
+let fruits = ["apple", "orange", "peach", "cherry"];
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+
+let favouriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak",
+};
+let { brian, anna, ...rest } = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
